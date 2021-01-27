2d59942869327a28b00e9f51f0a9e19885696ebb
Fengdi Liu <f3liu@ucsd.edu>
2021-01-27 12:20:24 -0800

    update

--- part1/part1-question19.js
+++ part1/part1-question19.js
@@ -1,1 +1,20 @@
+function modifyArray(array, callback){
+const newArr = [];
+for(let i=0; i<array.length; i++){
+  newArr.push(callback(array[i],function(x){
+    return x *2;
+  }));
+}
 
+return newArr;
+ 
+
+}
+
+function doSomething(num, callback){
+  return callback(num+2);
+}
+
+//result = modifyArray([1,2,3], doSomething);
+
+console.log(modifyArray([1,2,3], doSomething));
--- part1/part1-question20.js
+++ part1/part1-question20.js
@@ -1,1 +1,10 @@
 
+function timer(){
+let d = new Date();
+let time = d.toLocaleTimeString();
+}
+
+var myVar = setInterval(timer,1000);
+
+console.log(myVar);
+  
