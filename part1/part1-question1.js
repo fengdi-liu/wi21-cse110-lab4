d8e50d54828c3bdfa361b4a50420d34a07b1fa2d
Fengdi Liu <f3liu@ucsd.edu>
2021-01-26 16:01:12 -0800

    c

--- part1/part1-question1.js
+++ part1/part1-question1.js
@@ -1,1 +1,17 @@
-console.log("Hello!")
+function discountPrices (prices, discount){
+    var discounted = [];
+    var finalPrice =0;
+
+    for(var i=0; i < prices.length; i++){
+      var discountedPrice =price[i]* (1-discount);
+      finalPrice = Math.round(discountedPrice *100)/100;
+      discounted. push(finalPrice);
+    }
+
+    console.log(i);
+    console.log(discountedPrice);
+    console.log(finalPrice);
+
+    return discounted;
+
+}
